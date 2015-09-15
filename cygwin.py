89a85ba780fb489f3821b59cb6b5a50d187afb17
Jon Turney <jon.turney@dronecode.org.uk>
2015-09-15 12:45:20 +0100

    More work

--- cygwin.py
+++ cygwin.py
@@ -5,8 +5,9 @@
 # XXX: make these settable via command line options, with defaults
 
 HOMEDIRS='/sourceware/cygwin-staging/home'
-#PKGMAINT='/www/sourceware/htdocs/cygwin/cygwin-pkg-maint'
-PKGMAINT='cygwin-pkg-maint'
+
+PKGMAINT='/www/sourceware/htdocs/cygwin/cygwin-pkg-maint'
+
 #FTP='/var/ftp/pub/cygwin'
 FTP='/var/ftp/pub/cygwin-test'
 
