0a0054ebf98a73202a28f8288ba1a029e61e71dc
Jon Turney <jon.turney@dronecode.org.uk>
2015-09-17 14:09:44 +0100

    Adjust output

--- package.py
+++ package.py
@@ -49,12 +49,9 @@ def read_packages(arch):
         elif (len(files) > 0) and (relpath.count(os.path.sep) > 1):
             print("no setup hint in %s but files %s" % (dirpath, str(files)))
 
-    print("%d packages" % len(packages))
-
     return packages
 
 if __name__ == "__main__":
     for arch in common_constants.ARCHES:
         packages = read_packages(arch)
-        for p in packages.keys():
-            print(p, len(packages[p].tars))
+        print("arch %s has %d packages" % (arch, len(packages)))
