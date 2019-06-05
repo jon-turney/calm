jon-turney/calm

commit 6b03c67d3a59a9f6687b734729d3c4fd7465bb97

Report a count of files being ignored, rather than every pathname being ignored
There can be a lot of files being ignored, so this is can be spammy.

calm/uploads.py
@@ -59,6 +59,7 @@ def scan(m, all_packages, arch, args):
     remove_success = []
     error = False
     mtimes = [('', 0)]
+    ignored = 0
 
     logging.debug('reading packages from %s' % (basedir))
 
@@ -139,18 +140,14 @@ def scan(m, all_packages, arch, args):
             if file_mtime > mtime:
                 if mtime == 0:
                     m.reminders_timestamp_checked = True
-                    lvl = logging.DEBUG
 
-                    # don't warn until file is at least REMINDER_GRACE old, and
-                    # if more than REMINDER_INTERVAL has elapsed since we warned
-                    # about files being ignored, warn again
-                    if ((file_mtime < (time.time() - REMINDER_GRACE)) and
-                        (time.time() > (m.reminder_time + REMINDER_INTERVAL))):
-                        lvl = logging.WARNING
+                    logging.debug("ignoring %s as there is no !ready" % fn)
+                    ignored += 1
+
+                    # don't warn until file is at least REMINDER_GRACE old
+                    if (file_mtime < (time.time() - REMINDER_GRACE)):
                         if not args.dryrun:
                             m.reminders_issued = True
-
-                    logging.log(lvl, "ignoring %s as there is no !ready" % fn)
                 else:
                     logging.warning("ignoring %s as it is newer than !ready" % fn)
                 files.remove(f)
@@ -231,6 +228,12 @@ def scan(m, all_packages, arch, args):
     # reset
     if args.dryrun:
         m.reminders_timestamp_checked = True
+
+    # if files are being ignored, and more than REMINDER_INTERVAL has elapsed
+    # since we warned about files being ignored, warn again
+    if ignored > 0 and m.reminders_issued:
+        if (time.time() > (m.reminder_time + REMINDER_INTERVAL)):
+            logging.warning("ignored %d files as there is no !ready" % ignored)
 
     return ScanResult(error, packages, move, vault, remove, remove_success)
 

commit bc056955516aaaabfa70f02c77e87ddca474bf93

Fix ignored files warning
I think this has been broken since 6b03c67d.  Setting reminders_issued
if the grace period has expired causes the timestamp file to get touched
every time, so it's never been long enough after the first time to
actually issue the warning.

Also increase the reminder period to a week.

calm/uploads.py
@@ -37,8 +37,8 @@ import time
 from . import common_constants
 from . import package
 
-# reminders will be issued daily
-REMINDER_INTERVAL = 60 * 60 * 24
+# reminders will be issued weekly
+REMINDER_INTERVAL = 60 * 60 * 24 * 7
 # reminders don't start to be issued until an hour after upload
 REMINDER_GRACE = 60 * 60
 
@@ -116,12 +116,10 @@ def scan(m, all_packages, arch, args):
                     m.reminders_timestamp_checked = True
 
                     logging.debug("ignoring %s as there is no !ready" % fn)
-                    ignored += 1
 
                     # don't warn until file is at least REMINDER_GRACE old
                     if (file_mtime < (time.time() - REMINDER_GRACE)):
-                        if not args.dryrun:
-                            m.reminders_issued = True
+                        ignored += 1
                 else:
                     logging.warning("ignoring %s as it is newer than !ready" % fn)
                 files.remove(f)
@@ -267,9 +265,11 @@ def scan(m, all_packages, arch, args):
 
     # if files are being ignored, and more than REMINDER_INTERVAL has elapsed
     # since we warned about files being ignored, warn again
-    if ignored > 0 and m.reminders_issued:
+    if ignored > 0:
         if (time.time() > (m.reminder_time + REMINDER_INTERVAL)):
             logging.warning("ignored %d files in %s as there is no !ready" % (ignored, arch))
+            if not args.dryrun:
+                m.reminders_issued = True
 
     return ScanResult(error, packages, move, vault, remove, remove_success)
 