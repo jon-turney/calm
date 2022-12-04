jon-turney/calm

commit 3282d5c98e7bbad81abe17bdf42507f330c35caf

Expire superseded test versions
Expire superseded test versions (that is: expire test versions where a
non-test version with a higher version number exists), unless disabled by
the 'keep-superseded-test' hint.

(For this to work as expected, don't unconditionally mark the version
assigned to the test: stability level as not to be expired, so it can be
expired if superseded).

calm/hint.py
@@ -74,6 +74,7 @@ hintkeys[override] = {
     'keep-count': 'val',
     'keep-count-test': 'val',
     'keep-days': 'val',
+    'keep-superseded-versions': 'noval',
     'disable-check': 'val',
     'replace-versions': 'val',
 }

TODO
@@ -11,4 +11,3 @@
 * report changes in override.hint like we used to for setup.hint
 * maintainers.py should only re-read cygwin-pkg-maint if it's changed
 * empty install packages should only come in two variants: no dependencies and in _obsolete category, or with dependencies and in 'meta' category
-* expire test versions superseded by a non-test version (unless keep-superseded-test: in override.hint?)

calm/package.py
@@ -1340,7 +1340,7 @@ def mark_package_fresh(packages, p, v):
 def stale_packages(packages):
     for pn, po in packages.items():
         # mark any versions used by stability levels as fresh
-        for level in ['curr', 'prev', 'test']:
+        for level in ['curr', 'prev']:
             if level in po.stability:
                 v = po.stability[level]
                 mark_package_fresh(packages, pn, v)
@@ -1364,6 +1364,9 @@ def stale_packages(packages):
 
         # mark as fresh the highest n test versions, where n is given by the
         # keep-count-test: override hint, (defaulting to DEFAULT_KEEP_COUNT_TEST)
+        #
+        # only consider versions not superseded by non-test versions (unless
+        # 'keep-superseded-test' is present).
         keep_count = int(po.override_hints.get('keep-count-test', common_constants.DEFAULT_KEEP_COUNT_TEST))
         for v in sorted(po.versions(), key=lambda v: SetupVersion(v), reverse=True):
             if 'test' in po.version_hints[v]:
@@ -1371,6 +1374,9 @@ def stale_packages(packages):
                     break
                 mark_package_fresh(packages, pn, v)
                 keep_count = keep_count - 1
+            else:
+                if 'keep-superseded-test' not in po.override_hints:
+                    break
 
         # mark as fresh all versions after the first one which is newer than
         # the keep-days: override hint, (defaulting to DEFAULT_KEEP_DAYS)

commit babffd1ee53003c589de0e1374e531c4380561e9

Allow correct keep-superseded-test: key name in hint
FIx a thinko in 3282d5c9, where the key we allowed in the hint parser
and the key we checked for in package expiry are different.

Fixes: 3282d5c9

calm/hint.py
@@ -90,7 +90,7 @@ hintkeys[override] = {
     'keep-count': 'val',
     'keep-count-test': 'val',
     'keep-days': 'val',
-    'keep-superseded-versions': 'noval',
+    'keep-superseded-test': 'noval',
     'disable-check': 'val',
     'replace-versions': 'val',
 }