jon-turney/calm

commit 0939d5bd86f4ad757dc230256e39246d35db1743

Add 'calm-tool vault'

calm/calm.py
@@ -314,11 +314,13 @@ def remove_stale_packages(args, packages, state):
     to_vault['noarch'] = MoveList()
     to_vault['src'] = MoveList()
 
+    vault_requests = db.vault_requests(args)
+
     for arch in common_constants.ARCHES:
         logging.debug("checking for stale packages for arch %s" % (arch))
 
         # find stale packages
-        to_vault[arch] = package.stale_packages(packages[arch])
+        to_vault[arch] = package.stale_packages(packages[arch], vault_requests)
 
         # remove stale packages from package set
         to_vault[arch].map(lambda p, f: package.delete(packages[arch], p, f))

calm/db.py
@@ -42,6 +42,12 @@ def connect(args):
     conn.execute('''CREATE TABLE IF NOT EXISTS historic_package_names
                     (name TEXT NOT NULL PRIMARY KEY
                     )''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS vault_requests
+                    (srcpackage TEXT NOT NULL,
+                     vr TEXT NOT NULL
+                    )''')
+
     conn.commit()
 
     return conn
@@ -71,3 +77,30 @@ def update_package_names(args, packages):
     # - names which the removed package provide:d
     # - other packages which might provide: the name of a removed package
     return (historic_names - current_names)
+
+
+#
+# vault requests made via 'calm-tool vault'
+#
+def vault_requests(args):
+    requests = {}
+
+    with connect(args) as conn:
+        conn.row_factory = sqlite3.Row
+
+        cur = conn.execute("SELECT * FROM vault_requests")
+        for row in cur.fetchall():
+            spkg = row['srcpackage']
+            if spkg not in requests:
+                requests[spkg] = set()
+            requests[spkg].add(row['vr'])
+
+        # remove all rows
+        cur = conn.execute("DELETE FROM vault_requests")
+
+    return requests
+
+
+def vault_request_add(args, p, v):
+    with connect(args) as conn:
+        conn.execute('INSERT INTO vault_requests (srcpackage, vr) VALUES (?,?)', (p, v))

calm/package.py
@@ -1545,7 +1545,7 @@ def mark_package_fresh(packages, p, v, mark=Freshness.fresh):
 SO_AGE_THRESHOLD_YEARS = 5
 
 
-def stale_packages(packages):
+def stale_packages(packages, vault_requests):
     certain_age = time.time() - (SO_AGE_THRESHOLD_YEARS * 365.25 * 24 * 60 * 60)
     logging.debug("cut-off date for soversion package to be considered old is %s" % (time.strftime("%F %T %Z", time.localtime(certain_age))))
 
@@ -1616,6 +1616,19 @@ def stale_packages(packages):
                     return Freshness.fresh
 
             mark = noretain_hint_mark
+
+        # - marked via 'calm-tool vault'
+        #
+        es = po.srcpackage(bv, suffix=False)
+        if es in vault_requests:
+            def vault_requests_mark(v):
+                if v in vault_requests[es]:
+                    logging.info("package '%s' version '%s' not retained due vault request" % (pn, v))
+                    return Freshness.conditional
+                else:
+                    return Freshness.fresh
+
+            mark = vault_requests_mark
 
         # mark any versions explicitly listed in the keep: override hint (unconditionally)
         for v in po.override_hints.get('keep', '').split():

calm/tool_util.py
@@ -1,0 +1,60 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023 Jon Turney
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import logging
+import os
+import re
+
+from . import common_constants
+from . import maintainers
+
+
+def split(pvr):
+    # split name and vr
+    match = re.match(r'^(.+?)-(\d.*)', pvr)
+    if not match:
+        logging.error("unable to determine package and version-release from '%s'" % (pvr))
+        return (None, None)
+
+    p = match.group(1)
+    vr = match.group(2)
+
+    return (p, vr)
+
+
+def permitted(p):
+    # check CYGNAME is a maintainer for package
+    cygname = os.environ.get('CYGNAME', None)
+
+    mlist = {}
+    mlist = maintainers.add_packages(mlist, common_constants.PKGMAINT, orphanMaint=common_constants.ORPHANMAINT)
+
+    if cygname not in mlist:
+        logging.error("'%s' is not a package maintainer" % (cygname))
+        return False
+
+    if p not in mlist[cygname].pkgs:
+        logging.error("package '%s' is not in the package list for maintainer '%s'" % (p, cygname))
+        return False
+
+    return True

calm/vault.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2020 Jon Turney
+# Copyright (c) 2023 Jon Turney
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,14 +24,15 @@
 import argparse
 import logging
 import os
-import re
 import sys
+import types
 
 from . import common_constants
+from . import db
 from . import tool_util
 
 
-def untest(pvr):
+def vault(pvr):
     p, vr = tool_util.split(pvr)
     if not p:
         return
@@ -39,44 +40,22 @@ def untest(pvr):
     if not tool_util.permitted(p):
         return
 
-    # remove '^test:' lines from any package and subpackage hints
-    removed = 0
-    total = 0
-    for arch in common_constants.ARCHES + ['noarch']:
-        for (dirpath, _subdirs, files) in os.walk(os.path.join(common_constants.FTP, arch, 'release', p)):
-            for f in files:
-                if re.match(r'.*-' + re.escape(vr) + '(|-src).hint$', f):
-                    total = total + 1
-                    fn = os.path.join(dirpath, f)
+    args = types.SimpleNamespace()
+    args.htdocs = os.path.join(common_constants.HTDOCS, 'packages')
 
-                    with open(fn) as fh:
-                        content = fh.read()
-
-                    if re.search(r'^test:', content, re.MULTILINE):
-                        content = re.sub(r'^test:\s*$', '', content, 0, re.MULTILINE)
-
-                        with open(fn, 'w') as fh:
-                            fh.write(content)
-
-                        logging.info("Removed test: label from %s" % os.path.relpath(fn, common_constants.FTP))
-                        removed = removed + 1
-
-    if removed == 0:
-        logging.error("'%s' is not marked test" % pvr)
-    else:
-        logging.info("%d out of %d hints for '%s' version '%s' modified" % (removed, total, p, vr))
+    db.vault_request_add(args, p, vr)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='remove test: hint')
+    parser = argparse.ArgumentParser(description='mark packages for vaulting')
     parser.add_argument('package', nargs='+', metavar='SPVR')
     (args) = parser.parse_args()
 
     logging.getLogger().setLevel(logging.INFO)
-    logging.basicConfig(format='untest: %(message)s')
+    logging.basicConfig(format='vault: %(message)s')
 
     for p in args.package:
-        untest(p)
+        vault(p)
 
 
 if __name__ == "__main__":

commit 6be5440ff6d88db80bf862d04504c74610f7ed7a

Fix invalid packageset after process_relarea
If post-stale removal package set validation fails, don't change the
packageset during process_relarea(). This makes it similar to
process_uploads, in that the packageset can't be changed to something
invalid.

(Scenario: vaulting is requested via 'vault' command, which triggers
process_relarea(), which applies any requested vaultings during it's
stale evaluation.  If that fails to validate, the packageset is broken
and any subsequent uploads will fail until after another relarea scan
takes place)

Fixes: 0939d5bd86f4 ("Add 'calm-tool vault'")

calm/calm.py
@@ -121,7 +121,11 @@ def process_relarea(args, state):
     # packages can be stale due to changes made directly in the release
     # area, so first check here if there are any stale packages to vault
     if args.stale:
-        stale_to_vault = remove_stale_packages(args, packages, state)
+        fresh_packages = {}
+        for arch in common_constants.ARCHES:
+            fresh_packages[arch] = package.merge(packages[arch])
+
+        stale_to_vault = remove_stale_packages(args, fresh_packages, state)
         if stale_to_vault:
             for arch in common_constants.ARCHES + ['noarch', 'src']:
                 logging.info("vaulting %d old package(s) for arch %s" % (len(stale_to_vault[arch]), arch))
@@ -129,6 +133,8 @@ def process_relarea(args, state):
         else:
             logging.error("error while evaluating stale packages")
             return None
+
+        packages = fresh_packages
 
     # clean up any empty directories
     if not args.dryrun: