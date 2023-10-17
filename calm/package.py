74e796870e2480cde4d14c622cef7369a8e10d2b
Jon Turney <jon.turney@dronecode.org.uk>
2023-10-17 13:37:14 +0100

    Expunge all python2 modules

--- calm/package.py
+++ calm/package.py
@@ -1636,10 +1636,10 @@ def mark_fn(packages, po, v, certain_age, vault_requests):
     # - if package depends on anything in expired_provides
     #
     requires = po.version_hints[v].get('depends', '').split(', ')
-    if re.match(r'^python(|2|27)-', pn):
+    if re.match(r'^python(|2|27)[-_]', pn):
         if any(ep in requires for ep in past_mistakes.expired_provides) or po.obsolete:
             logging.debug("package '%s' version '%s' not retained as it requires a provide known to be expired" % (pn, v))
-            return Freshness.conditional
+            return Freshness.stale
 
     # - explicitly marked as 'noretain'
     #
