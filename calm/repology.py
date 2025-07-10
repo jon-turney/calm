jon-turney/calm

commit 4ec72855466328f24ce594c4a333a2414675fac3

Handle multiple 'newest' versions reported by repology
repology can now report multiple 'newest' versions, due to altver
schemes. Handle that better by employing a simple heuristic (equal
number of alphanumeric sequences) to find which one matches our version.

calm/repology.py
@@ -82,15 +82,15 @@ def repology_fetch_data():
         for pn in sorted(j.keys()):
             p = j[pn]
 
-            # first, pick out the version which repology has called newest, and
+            # first, pick out the version(s) which repology has called newest, and
             # if needed, also pick out latest version for legacy packages
-            newest_version = None
+            newest_version = []
             legacy_versions = {}
 
             for i in p:
                 v = i['version']
                 if i['status'] == 'newest':
-                    newest_version = v
+                    newest_version.append(v)
 
                 if (pn in use_legacy) and (i['status'] in ['legacy', 'outdated']):
                     prefix = None
@@ -115,6 +115,9 @@ def repology_fetch_data():
             if not newest_version:
                 continue
 
+            # XXX: if everything is noscheme, we can probably record that
+            # information as well.
+
             # next, assign that version to all the corresponding cygwin source
             # packages
             #
@@ -134,7 +137,7 @@ def repology_fetch_data():
                         if prefix and prefix in legacy_versions:
                             upstream_version = legacy_versions[prefix]
                     else:
-                        # otherwise, just use the newest version
+                        # otherwise, just use the newest version(s)
                         upstream_version = newest_version
 
                     repology_data[source_pn] = RepologyData(upstream_version, pn)
@@ -148,6 +151,20 @@ def repology_fetch_data():
         time.sleep(1)
 
     return repology_data
+
+
+# when repology reports multiple 'newest' versions due to altver, pick the one
+# which splits into the same number of numeric and alphabetic sequences
+def seqmatch(bv, uv):
+    if len(uv) <= 1:
+        return uv[0]
+
+    seq_count = len(SetupVersion(bv)._V)
+    for v in uv:
+        if len(SetupVersion(bv)._V) == seq_count:
+            return v
+
+    return uv[0]
 
 
 def annotate_packages(args, packages):
@@ -167,7 +184,7 @@ def annotate_packages(args, packages):
         spn = pn + '-src'
         for arch in packages:
             if spn in packages[arch]:
-                packages[arch][spn].upstream_version = last_data[pn].upstream_version
+                packages[arch][spn].upstream_version = seqmatch(packages[arch][spn].best_version, last_data[pn].upstream_version)
                 packages[arch][spn].repology_project_name = last_data[pn].repology_project_name
 
     last_check = time.time()

commit b24400eb1825ccd672237d2e9250421a1eeaca8e

Generalize getting versions for slotted packages from repology
Add automake to the list of slotted packages.

(This also fixed the legacy package version being reported correctly,
rather than jsut the first character, as we didn't turn it into a list
when we added support for repology reporting multiple newest versions)

Fixes: 4ec728554663 ("Handle multiple 'newest' versions reported by repology")

calm/repology.py
@@ -28,6 +28,7 @@
 
 import json
 import logging
+import re
 import time
 import urllib.error
 import urllib.request
@@ -40,18 +41,13 @@ REPOLOGY_API_URL = 'https://repology.org/api/v1/projects/'
 last_check = 0
 last_data = {}
 
-LegacyData = namedtuple('LegacyData', ['version', 'ignores'])
-use_legacy = {'qt': [LegacyData('5', []),
-                     LegacyData('4', []),
-                     LegacyData('3', [])],
-              'gtk': [LegacyData('3', ['3.9', '+']),
-                      LegacyData('2', [])],
-              'gtksourceview': [LegacyData('2', []),
-                                LegacyData('3', []),
-                                LegacyData('4', []),
-                                LegacyData('5', []),
-                                ]
-              }
+LegacyData = namedtuple('LegacyData', ['version_re', 'ignores'])
+use_legacy = {
+    'automake': LegacyData(r'\d.\d+', ['+']),
+    'gtk': LegacyData(r'\d', ['3.9', '+', '-']),
+    'gtksourceview': LegacyData(r'\d', []),
+    'qt': LegacyData(r'\d', ['p']),
+}
 
 RepologyData = namedtuple('RepologyData', ['upstream_version', 'repology_project_name'])
 
@@ -105,17 +101,17 @@ def repology_fetch_data():
                     newest_version.append(v)
 
                 if (pn in use_legacy) and (i['status'] in ['legacy', 'outdated']):
-                    prefix = None
-                    for ld in use_legacy[pn]:
-                        if v.startswith(ld.version):
-                            prefix = ld.version
-                            break
+                    ld = use_legacy[pn]
+                    prefix_re = ld.version_re
 
-                    if not prefix:
+                    m = re.match(r'^(' + prefix_re + r')', v)
+                    if m:
+                        prefix = m.group(1)
+                    else:
                         continue
 
-                    # blacklist versions containing substrings (pre-release
-                    # versions etc.)
+                    # blacklist versions containing certain substrings
+                    # (pre-release versions etc.)
                     if any(ignore in v for ignore in ld.ignores):
                         continue
 
@@ -153,12 +149,16 @@ def repology_fetch_data():
                     # if package name contains legacy version
                     if pn in use_legacy:
                         prefix = None
-                        for ld in use_legacy[pn]:
-                            if (pn + ld.version) in source_pn:
-                                prefix = ld.version
+                        for p in legacy_versions:
+                            if (pn + p) in source_pn:
+                                prefix = p
+                                break
 
-                        if prefix and prefix in legacy_versions:
-                            upstream_version = legacy_versions[prefix]
+                        if prefix:
+                            upstream_version = [legacy_versions[prefix]]
+                        else:
+                            upstream_version = newest_version
+
                     else:
                         # otherwise, just use the newest version(s)
                         upstream_version = newest_version